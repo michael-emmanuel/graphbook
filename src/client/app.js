c242d64d25545ea44728017a301809fba50e68ca
Michael Emmanuel <mike@Michaels-MBP-3.attlocal.net>
2023-10-12 15:13:54 -0400

    set up working React env

--- src/client/app.js
+++ src/client/app.js
@@ -1,7 +1,76 @@
-import React from 'react';
+import { Helmet } from 'react-helmet';
+import React, { useState } from 'react';
+import '../../assets/css/style.css';
+
+const initialPosts = [
+  {
+    id: 2,
+    text: 'Lorem ipsum',
+    user: {
+      avatar: '/uploads/avatar1.png',
+      username: 'Test User',
+    },
+  },
+  {
+    id: 1,
+    text: 'Lorem ipsum',
+    user: {
+      avatar: '/uploads/avatar2.png',
+      username: 'Test User 2',
+    },
+  },
+];
 
 const App = () => {
-  return <div>Hello World!</div>;
+  const [posts, setPosts] = useState(initialPosts);
+  const [postContent, setPostContent] = useState('');
+
+  return (
+    <div className='container'>
+      <Helmet>
+        <title>Graphbook - Feed</title>
+        <meta
+          name='description'
+          content='Newfeed of all your friends on Graphbook'
+        />
+      </Helmet>
+      <div className='postForm'>
+        <form onSubmit={handleSubmit}>
+          <textarea
+            value={postContent}
+            onChange={e => setPostContent(e.target.value)}
+            placeholder='Write your custom post!'
+          />
+          <input type='submit' value='Submit' />
+        </form>
+      </div>
+      <div className='feed'>
+        {initialPosts.map((post, i) => (
+          <div key={post.id} className='post'>
+            <div className='header'>
+              <img src={post.user.avatar} />
+              <h2>{post.user.username}</h2>
+            </div>
+            <p className='content'>{post.text}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    const newPost = {
+      id: posts.length + 1,
+      text: postContent,
+      user: {
+        avatar: '/uploads/avatar1.png',
+        username: 'Fake User',
+      },
+    };
+    setPosts([newPost, ...posts]);
+    setPostContent('');
+  };
 };
 
 export default App;
