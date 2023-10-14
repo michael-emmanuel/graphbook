87369ed92171a77a044ec088cb35071c8ec72a26
Michael Emmanuel <mike@Michaels-MacBook-Pro-3.local>
2023-10-14 17:02:03 -0400

    configured graphql to return fake post data

--- src/server/index.js
+++ src/server/index.js
@@ -1,25 +1,31 @@
 import express from 'express';
+import path from 'path';
 import helmet from 'helmet';
 import cors from 'cors';
 import compress from 'compression';
-
-import path from 'path';
+import services from './services';
 
 const root = path.join(__dirname, '../../');
 
 const app = express();
+app.use(compress());
 
-app.use(helmet());
-app.use(
-  helmet.contentSecurityPolicy({
-    directives: {
-      defaultSrc: ['self'],
-      scriptSrc: ["'self'", "'unsafe-inline'"],
-      styleSrc: ["'self'", "'unsafe-inline'"],
-      imgSrc: ["'self'", 'data:', '*.amazonaws.com'],
-    },
-  })
-);
+if (process.env.NODE_ENV === 'production') {
+  app.use(helmet());
+
+  app.use(
+    helmet.contentSecurityPolicy({
+      directives: {
+        defaultSrc: ['self'],
+        scriptSrc: ["'self'", "'unsafe-inline'"],
+        styleSrc: ["'self'", "'unsafe-inline'"],
+        imgSrc: ["'self'", 'data:', '*.amazonaws.com'],
+      },
+    })
+  );
+}
+
+app.use(cors());
 
 app.use('/', express.static(path.join(root, 'dist/client')));
 
@@ -28,5 +34,19 @@ app.use('/uploads', express.static(path.join(root, 'uploads')));
 app.get('/', (req, res) => {
   res.sendFile(path.join(root, '/dist/client/index.html'));
 });
+
+const serviceNames = Object.keys(services);
+
+for (let i = 0; i < serviceNames.length; i += 1) {
+  const name = serviceNames[i];
+  if (name === 'graphql') {
+    (async () => {
+      await services[name].start();
+      services[name].applyMiddleware({ app });
+    })();
+  } else {
+    app.use('/${name}', services[name]);
+  }
+}
 
 app.listen(8000, () => console.log('Listening on port 8000!'));
---
+++ src/server/services/graphql/resolvers.js
@@ -0,0 +1,28 @@
+const posts = [
+  {
+    id: 2,
+    text: 'Lorem ipsum',
+    user: {
+      avatar: '/uploads/avatar1.png',
+      username: 'Test User',
+    },
+  },
+  {
+    id: 1,
+    text: 'Lorem ipsum',
+    user: {
+      avatar: '/uploads/avatar2.png',
+      username: 'Test User 2',
+    },
+  },
+];
+
+const resolvers = {
+  RootQuery: {
+    posts(roots, args, context) {
+      return posts;
+    },
+  },
+};
+
+export default resolvers;
